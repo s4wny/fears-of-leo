1739ba3524c97503cc6a0b14b297ad69018cac07
Filip Lindvall <drpytho@gmail.com>
2016-11-12 10:52:19 +0100

    Fixed something

--- server/index.js
+++ server/index.js
@@ -7,16 +7,17 @@ var dungeon = new Dungeon({
     rooms: {
         any: {
             min_size: [2, 3],
-            max_size: [5, 6]
+            max_size: [5, 6],
+            max_exits: 4
         }
     },
-    max_corridor_length: 6,
+    max_corridor_length: 10,
     min_corridor_length: 2,
     corridor_density: 0.5, //corridors per room 
     symmetric_rooms: false, // exits must be in the center of a wall if true 
     interconnects: 1, //extra corridors to connect rooms and make circular paths. not 100% guaranteed 
     max_interconnect_length: 10,
-    room_count: 10
+    room_count: Math.floor(Math.random()*10)+4
 });
 
 dungeon.generate();
